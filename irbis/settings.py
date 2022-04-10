f6cd9b6dcb93678cc559a3545cab6f9a68d70b65
Rom1987 <maks3564195@gmail.com>
2022-04-10 14:34:17 +0300

    10.04.2022 first commit

--- irbis/settings.py
+++ irbis/settings.py
@@ -132,8 +132,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'irbis/static'),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'photos')
+MEDIA_URL = '/photos/'
 
 INTERNAL_IPS = ['127.0.0.1', ]
 
